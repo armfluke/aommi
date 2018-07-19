0b288da2a0ec3a1e1efbbb942be3e693a2111f56
Armfluke <armfluke@hotmail.com>
2018-07-19 16:39:09 +0700

    [Edited]: modify promotion

--- API/src/loyalty/promotion.go
+++ API/src/loyalty/promotion.go
@@ -1,8 +1,12 @@
 package loyalty
 
 import (
+	"database/sql"
 	"encoding/json"
 	"net/http"
+	"strconv"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type Promotion struct {
@@ -14,11 +18,138 @@ type Promotion struct {
 	Image         string `json:"image"`
 }
 
+type PromotionUsed struct {
+	AccountID   string `json:"accountID"`
+	PromotionID int    `json:"promotionID"`
+	RewardCode  string `json:"rewardCode"`
+	Date        string `json:"date"`
+}
+
+type Status struct {
+	StatusCode int    `json:"statusCode"`
+	Status     string `json:"status"`
+}
+
 func GetPromotion(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
-	msg := Promotion{PromotionID: 0, PromotionName: "", Reward: "", Condition: "", Point: 0, Image: ""}
+	//message := Promotion{PromotionID: 1, PromotionName: "ตั๋วหนังฟรีMajor", Reward: "ตั๋วหนังMajor", Condition: "", Point: 100, Image: ""}
+	message := GetPromotionFromDatabase()
+
+	w.Write([]byte(message))
+}
+
+func UsePromotion(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	var promotion PromotionUsed
+
+	body := json.NewDecoder(r.Body)
+	body.Decode(&promotion)
+
+	db, err := sql.Open("mysql", "root:Admin123!@tcp(178.128.48.140:3306)/aommi")
+
+	if err != nil {
+		message := Status{StatusCode: 500, Status: "Can't connect to database"}
+
+		encoder := json.NewEncoder(w)
+		encoder.Encode(message)
+	}
+
+	defer db.Close()
+
+	results, err := db.Query("INSERT INTO promotionUsed (accountID, promotionID, rewardCode) VALUES ('" + promotion.AccountID + "'," + strconv.Itoa(promotion.PromotionID) + ",'" + promotion.RewardCode + "')")
+	results.Close()
+
+	if err != nil {
+		message := Status{StatusCode: 500, Status: "Can't insert to database"}
+
+		encoder := json.NewEncoder(w)
+		encoder.Encode(message)
+	}
+
+	message := Status{StatusCode: 200, Status: "ok"}
 
 	encoder := json.NewEncoder(w)
-	encoder.Encode(msg)
+	encoder.Encode(message)
+}
+
+func GetPromotionFromDatabase() string {
+	db, err := sql.Open("mysql", "root:Admin123!@tcp(178.128.48.140:3306)/aommi")
+
+	if err != nil {
+		return ""
+	}
+
+	defer db.Close()
+
+	rows, err := db.Query("SELECT * FROM promotion")
+	if err != nil {
+		return ""
+	}
+
+	columns, err := rows.Columns()
+	if err != nil {
+		return ""
+	}
+
+	values := make([]sql.RawBytes, len(columns))
+
+	scanArgs := make([]interface{}, len(values))
+	for i := range values {
+		scanArgs[i] = &values[i]
+	}
+
+	var promotion []Promotion
+
+	for rows.Next() {
+		err = rows.Scan(scanArgs...)
+		if err != nil {
+			return ""
+		}
+
+		var value string
+		var data Promotion
+		for i, col := range values {
+			if col == nil {
+				value = "NULL"
+			} else {
+				value = string(col)
+			}
+
+			switch columns[i] {
+			case "PromtotionID":
+				data.PromotionID, _ = strconv.Atoi(value)
+			case "PromotionName":
+				data.PromotionName = value
+			case "Reward":
+				data.Reward = value
+			case "Redeem_condition":
+				data.Condition = value
+			case "Point":
+				data.Point, _ = strconv.Atoi(value)
+			case "Image":
+				data.Image = value
+			}
+
+			if i == len(columns)-1 {
+				promotion = append(promotion,
+					Promotion{
+						PromotionID:   data.PromotionID,
+						PromotionName: data.PromotionName,
+						Reward:        data.Reward,
+						Condition:     data.Condition,
+						Point:         data.Point,
+						Image:         data.Image,
+					})
+			}
+		}
+	}
+
+	if err = rows.Err(); err != nil {
+		return ""
+	}
+
+	arrPromotion, _ := json.Marshal(promotion)
+	return string(arrPromotion)
 }
--- API/src/loyalty/promotion_test.go
+++ API/src/loyalty/promotion_test.go
@@ -10,7 +10,7 @@ import (
 )
 
 func Test_Get_Promotion_Should_Return_All_Promotion(t *testing.T) {
-	expected := `{"promotionID":1,"promotionName":"ตั๋วหนังฟรีMajor","reward":"ตั๋วหนังMajor","condition":"","point":100,"image":""}`
+	expected := `[{"promotionID":0,"promotionName":"ตั๋วหนัังฟรีMajor","reward":"ตั๋วหนังMajor","condition":"","point":100,"image":""},{"promotionID":0,"promotionName":"Starbuck voucher","reward":"Starbuck voucher 100 bath","condition":"","point":200,"image":""},{"promotionID":0,"promotionName":"Digital Salak","reward":"Digital Salak 1 unit","condition":"","point":100,"image":""},{"promotionID":0,"promotionName":"Major movie ticket","reward":"Major movie 1 unit","condition":"","point":300,"image":""},{"promotionID":0,"promotionName":"SF movie ticket","reward":"SF movie 1 unit","condition":"","point":300,"image":""},{"promotionID":0,"promotionName":"True Coffee voucher","reward":"True Coffee voucher 100 bath","condition":"","point":200,"image":""},{"promotionID":0,"promotionName":"Central gift voucher","reward":"Central gift voucher 100 bath","condition":"","point":200,"image":""},{"promotionID":0,"promotionName":"Bag","reward":"Bag","condition":"","point":400,"image":""}]`
 	r, _ := http.NewRequest(http.MethodGet, "/promotion", nil)
 	w := httptest.NewRecorder()
 
@@ -21,7 +21,7 @@ func Test_Get_Promotion_Should_Return_All_Promotion(t *testing.T) {
 	actual := strings.TrimSpace(string(body))
 
 	if actual != expected {
-		t.Errorf("expect '%s' but got '%s'", expected, actual)
+		t.Errorf("expect\n'%s' but got\n'%s'", expected, actual)
 	}
 }
 
@@ -42,4 +42,14 @@ func Test_Promotion_Json_Input_PromotionID_1_AccountID_1100400758552_Rewardcode_
 		t.Errorf("expect '%s' but got '%s'", expected, actual)
 	}
 
+}
+
+func Test_ActionToTest_Input_JSON_Should_Be_PromotionJSON(t *testing.T) {
+	expectedResult := `[{"promotionID":0,"promotionName":"ตั๋วหนัังฟรีMajor","reward":"ตั๋วหนังMajor","condition":"","point":100,"image":""},{"promotionID":0,"promotionName":"Starbuck voucher","reward":"Starbuck voucher 100 bath","condition":"","point":200,"image":""},{"promotionID":0,"promotionName":"Digital Salak","reward":"Digital Salak 1 unit","condition":"","point":100,"image":""},{"promotionID":0,"promotionName":"Major movie ticket","reward":"Major movie 1 unit","condition":"","point":300,"image":""},{"promotionID":0,"promotionName":"SF movie ticket","reward":"SF movie 1 unit","condition":"","point":300,"image":""},{"promotionID":0,"promotionName":"True Coffee voucher","reward":"True Coffee voucher 100 bath","condition":"","point":200,"image":""},{"promotionID":0,"promotionName":"Central gift voucher","reward":"Central gift voucher 100 bath","condition":"","point":200,"image":""},{"promotionID":0,"promotionName":"Bag","reward":"Bag","condition":"","point":400,"image":""}]`
+
+	actualResult := GetPromotionFromDatabase()
+
+	if expectedResult != actualResult {
+		t.Errorf("expected\n%s but got\n%s", expectedResult, actualResult)
+	}
 }
