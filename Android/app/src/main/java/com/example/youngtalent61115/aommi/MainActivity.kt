1899d7da06c97be2d78ff52bffc77c0bc1bd444c
beerbeatbox4 <beer.beatbox.work@gmail.com>
2018-07-19 12:19:45 +0700

    [Edited]: add recyclerView Promotion List

--- Android/app/src/main/java/com/example/youngtalent61115/aommi/MainActivity.kt
+++ Android/app/src/main/java/com/example/youngtalent61115/aommi/MainActivity.kt
@@ -2,11 +2,26 @@ package com.example.youngtalent61115.aommi
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val promotion: ArrayList<String> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        addPromotions()
+
+        rcvPromotionList.layoutManager = LinearLayoutManager(this)
+
+        rcvPromotionList.adapter = PromotionAdapter(promotion, this)
+
     }
+
+    fun addPromotions() {
+        promotion.add("หลวงพี่แจ๊ส 5G")
+        promotion.add("หลวงพี่แจ๊ส 6G") }
 }
