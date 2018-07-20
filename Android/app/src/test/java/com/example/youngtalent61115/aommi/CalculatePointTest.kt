98346e1fbee9838f771c3e34c7bb8ef291d52eeb
Pawarisorn <Pawarisornmayanon@hotmail.com>
2018-07-20 11:49:52 +0700

    [Edited] : Created test funtion genarate code for redeem

--- Android/app/src/test/java/com/example/youngtalent61115/aommi/CalculatePointTest.kt
+++ Android/app/src/test/java/com/example/youngtalent61115/aommi/CalculatePointTest.kt
@@ -11,5 +11,12 @@ class CalculatePointTest {
         val actualResult = rewardCalculate.decreasePoint(1000, 100)
         assertEquals(expectedResult, actualResult)
     }
+    @Test
+    fun generatrQRcodeOfRedeemInputNoneShuldbeRedeemCodeRandomGenerate9Position(){
+        val redeemCode = RewardActivity()
+        val expectedResult = 9
+        val actualResult =  redeemCode.generateCode(8).length
+        assertEquals(expectedResult,actualResult)
+    }
 }
 
