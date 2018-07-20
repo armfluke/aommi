0411319aeacb6228bad2047718084edad23b42ae
beerbeatbox4 <beer.beatbox.work@gmail.com>
2018-07-20 10:29:23 +0700

    [Edited]: change package of RewardActivity

--- Android/app/src/main/java/com/example/youngtalent61115/aommi/MainActivity.kt
+++ Android/app/src/main/java/com/example/youngtalent61115/aommi/MainActivity.kt
@@ -4,7 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
-import android.util.Log
+import com.example.youngtalent61115.aommi.activity.RewardActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
