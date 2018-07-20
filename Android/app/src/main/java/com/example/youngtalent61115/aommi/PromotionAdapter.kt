d8ef0bacc5f570f2281408e80a3519add8760f75
beerbeatbox4 <beer.beatbox.work@gmail.com>
2018-07-20 09:51:25 +0700

    [Edited]: rename variable

--- Android/app/src/main/java/com/example/youngtalent61115/aommi/PromotionAdapter.kt
+++ Android/app/src/main/java/com/example/youngtalent61115/aommi/PromotionAdapter.kt
@@ -3,26 +3,28 @@ package com.example.youngtalent61115.aommi
 import android.content.Context
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.promotion_list_item.view.*
 
-class PromotionAdapter(val items : ArrayList<String>, val context: Context) : RecyclerView.Adapter<ViewHolder>() {
+class PromotionAdapter(val list: ArrayList<String>, val context: Context) : RecyclerView.Adapter<ViewHolder>() {
     override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
+
         return ViewHolder(LayoutInflater.from(context).inflate(R.layout.promotion_list_item, p0, false))
     }
 
     override fun getItemCount(): Int {
-        return items.size
+        return list.size
     }
 
     override fun onBindViewHolder(p0: ViewHolder, p1: Int) {
-        p0?.tvPromotionName?.text = items.get(p1)
+        p0?.tvPromotionName?.text = list[p1]
     }
 
 }
 
-class ViewHolder (view: View) : RecyclerView.ViewHolder(view) {
-    // Holds the TextView that will add each animal to
-        val tvPromotionName = view.tvPromotionName
+class ViewHolder (view: View) : RecyclerView.ViewHolder(view){
+    val tvPromotionName = view.tvPromotionName
+
 }
