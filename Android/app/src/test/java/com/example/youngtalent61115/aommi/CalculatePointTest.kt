cf991a6c0cee185881d92ebd9e02af0bfdc6a4ab
Pawarisorn <Pawarisornmayanon@hotmail.com>
2018-07-20 10:44:27 +0700

    [Created] : Make file CalculatePointTest.kt, It is part of decreas piont between currnt point and point of reward

---
+++ Android/app/src/test/java/com/example/youngtalent61115/aommi/CalculatePointTest.kt
@@ -0,0 +1,15 @@
+package com.example.youngtalent61115.aommi
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class CalculatePointTest {
+    @Test
+    fun decreasePointInputPointBalance1000AndPointReward100ShuldbePointBlance900Test() {
+        val rewardCalculate = RewardActivity()
+        val expectedResult = 900
+        val actualResult = rewardCalculate.decreasePoint(1000, 100)
+        assertEquals(expectedResult, actualResult)
+    }
+}
+
