0f225fa50e4724e6d928bf74a6b0bfbf7a56bbcb
Akkrawut Khanthawan <akkrawut33588@gmail.com>
2018-07-20 12:54:25 +0700

    [Created]: function and test point updating

--- API/src/main.go
+++ API/src/main.go
@@ -10,6 +10,7 @@ func main() {
 	http.HandleFunc("/account", loyalty.GetAccount)
 	http.HandleFunc("/promotion", loyalty.GetPromotion)
 	http.HandleFunc("/promotion/use", loyalty.UsePromotion)
+	http.HandleFunc("/point/update", loyalty.UpdatePoint)
 
 	log.Println("Server running on port 3000")
 
