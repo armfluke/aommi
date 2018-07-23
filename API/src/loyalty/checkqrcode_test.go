e7855864144421f37a50a4890565c69d666fb43b
Akkrawut Khanthawan <akkrawut33588@gmail.com>
2018-07-23 15:56:02 +0700

    [Created]: test checkqrcode and [Edited]: checkqrcode and updatepoint

---
+++ API/src/loyalty/checkqrcode_test.go
@@ -0,0 +1,33 @@
+package loyalty_test
+
+import (
+	. "loyalty"
+	"testing"
+)
+
+func Test_CheckQRCode_Input_qr_200savingaccount_and_account1969900224728_Should_Be_Banlance_and_point(t *testing.T) {
+	qrCode := "200|saving_account"
+	account := "1969900224728"
+	actual_balance, actual_point, _ := UpdatePointByQR(qrCode, account)
+	balance, _ := GetBalanceAccount(account)
+	point, _ := GetPointQR(qrCode)
+	expected_balance, expected_point := balance, point
+
+	if actual_balance != expected_balance || actual_point != expected_point {
+		t.Errorf("expect '%d %d' but got '%d %d'", expected_balance, expected_point, actual_balance, actual_point)
+	}
+}
+
+func Test_GetQRCodeFromDatabase_Input_qr_200savingaccount_and_account1969900224728_Should_Be_Banlance_and_point(t *testing.T) {
+	qrCode := "200|saving_account"
+	account := "1969900224728"
+	actual_balance, actual_point, _ := UpdatePointByQR(qrCode, account)
+	balance, _ := GetBalanceAccount(account)
+	point, _ := GetPointQR(qrCode)
+	expected_balance, expected_point := balance, point
+
+	if actual_balance != expected_balance || actual_point != expected_point {
+		t.Errorf("expect '%d %d' but got '%d %d'", expected_balance, expected_point, actual_balance, actual_point)
+	}
+
+}
--- API/src/loyalty/updatePointByQR.go
+++ API/src/loyalty/updatePointByQR.go
@@ -1,24 +1,20 @@
 package loyalty
 
-import (
-	
-)
+func UpdatePointByQR(qr string, account string) (int, int, bool) {
 
-func UpdatePointByQR(qr string, account string) (int,int,bool){
-
-	pointqr,status := GetPointQR(qr)
+	pointqr, status := GetPointQR(qr)
 	resultbalance := 0
 	if !status {
-		return 0,0,false
+		return 0, 0, false
 	}
-	accountbalance,status := GetBalanceAccount(account)
+	accountbalance, status := GetBalanceAccount(account)
 	if !status {
-		return 0,0,false
+		return 0, 0, false
 	}
-	resultbalance = accountbalance+pointqr
-	if updatePointToDatabase(resultbalance,account){
-		return accountbalance,pointqr,true
-	}else {
-		return 0,0,false
+	resultbalance = accountbalance + pointqr
+	if updatePointToDatabase(resultbalance, account) {
+		return resultbalance, pointqr, true
+	} else {
+		return 0, 0, false
 	}
 }
