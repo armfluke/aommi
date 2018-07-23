c0e0bc91b4b627afe8cc30ff2fdc03c39483e9e6
beerbeatbox4 <beer.beatbox.work@gmail.com>
2018-07-23 14:37:37 +0700

    [Edited]: set properties scanQR and open scanQR

--- Android/app/src/main/java/com/example/youngtalent61115/aommi/ScanQRActivity.kt
+++ Android/app/src/main/java/com/example/youngtalent61115/aommi/ScanQRActivity.kt
@@ -2,11 +2,67 @@ package com.example.youngtalent61115.aommi
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.budiyev.android.codescanner.AutoFocusMode
+import com.budiyev.android.codescanner.CodeScanner
+import com.budiyev.android.codescanner.DecodeCallback
+import com.budiyev.android.codescanner.ErrorCallback
+import com.budiyev.android.codescanner.ScanMode
+import kotlinx.android.synthetic.main.activity_scan_qr.*
 
 class ScanQRActivity : AppCompatActivity() {
+
+    private lateinit var codeScanner: CodeScanner
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scan_qr)
+
+        setScannerProperties()
+        callbackFromScanner()
+
+        clickScannerToContinual()
+    }
+
+    private fun clickScannerToContinual() {
+        scanner_view.setOnClickListener {
+            codeScanner.startPreview()
+        }
+    }
+
+    private fun callbackFromScanner() {
+        codeScanner.decodeCallback = DecodeCallback {
+            runOnUiThread {
+                //Open ShowPoint
+                Toast.makeText(this, "Scan result: ${it.text}", Toast.LENGTH_LONG).show()
+            }
+        }
+        codeScanner.errorCallback = ErrorCallback {
+            runOnUiThread {
+                Toast.makeText(this, "Camera initialization error: ${it.message}",
+                        Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    private fun setScannerProperties() {
+        codeScanner = CodeScanner(this, scanner_view)
+
+        codeScanner.camera = CodeScanner.CAMERA_BACK
+        codeScanner.formats = CodeScanner.ALL_FORMATS
+        codeScanner.autoFocusMode = AutoFocusMode.SAFE
+        codeScanner.scanMode = ScanMode.SINGLE
+        codeScanner.isAutoFocusEnabled = true
+        codeScanner.isFlashEnabled = false
+    }
+
+    override fun onResume() {
+        super.onResume()
+        codeScanner.startPreview()
+    }
+
+    override fun onPause() {
+        codeScanner.releaseResources()
+        super.onPause()
     }
 }
