d0557e5728bd9cedbfaab2d842714851f2b161ed
ap08960695 <big_xxkool@hotmail.com>
2018-07-24 16:36:24 +0700

    [Created]: func and test gethistory and webViewAccount

---
+++ API/src/loyalty/gethistory.go
@@ -0,0 +1,22 @@
+package loyalty
+
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type PostHistoryData struct{
+	AccountID string `json:accountID`
+}
+
+func GetHistory(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	var data PostHistoryData
+	body := json.NewDecoder(r.Body)
+	body.Decode(&data)
+
+	message := GetHistoryFromDatabase(data.AccountID)
+	w.Write([]byte(message))
+}
