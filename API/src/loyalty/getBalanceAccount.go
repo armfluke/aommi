9eccc7ef9eae2f6ccb977426f041ee86b7e34c76
Akkrawut Khanthawan <akkrawut33588@gmail.com>
2018-07-24 15:11:02 +0700

    [Created]: test getidqr getpointqr [Edited]: refactor account

--- API/src/loyalty/getBalanceAccount.go
+++ API/src/loyalty/getBalanceAccount.go
@@ -4,27 +4,27 @@ import (
 	"fmt"
 )
 
-func GetBalanceAccount(account string) (int,bool){
-	db := ConnectDatabase();
-	if db==nil{
-		return -1,false
+func GetBalanceAccount(account string) (int, bool) {
+	db := ConnectDatabase()
+	if db == nil {
+		return -1, false
 	}
 
 	defer db.Close()
-	queryString := fmt.Sprintf("SELECT PointBalance FROM account WHERE AccountID='%s'",account)
+	queryString := fmt.Sprintf("SELECT PointBalance FROM account WHERE AccountID='%s'", account)
 	rows, _ := db.Query(queryString)
 	if err := rows.Err(); err != nil {
-		return -1,false
+		return -1, false
 	}
 	balance := 0
 	if rows.Next() {
 		if err := rows.Scan(&balance); err != nil {
-			return -1,false	
+			return -1, false
 		}
-		return balance,true
-	}else{
-		return balance,false
+		return balance, true
+	} else {
+		return balance, false
 	}
 	rows.Close()
-	return balance,false	
+	return balance, false
 }
