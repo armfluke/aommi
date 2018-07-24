bc1768ac728b692d75dfc57422b54c0bb49969db
Akkrawut Khanthawan <akkrawut33588@gmail.com>
2018-07-24 10:27:25 +0700

    [Edited]: file avout bussiness condition

--- API/src/loyalty/setuseqr.go
+++ API/src/loyalty/setuseqr.go
@@ -2,34 +2,39 @@ package loyalty
 
 import (
 	"fmt"
+	"strings"
 )
 
-func SetUseQR(account string, qr string,point int) bool{
-	
-	db := ConnectDatabase();
-	if db==nil{
+func SetUseQR(account string, qr string, point int) bool {
+
+	db := ConnectDatabase()
+	if db == nil {
 		return false
 	}
 	defer db.Close()
-	if qr.Split(qr,"|")[1]=="saving_account"{
-		rows, error := db.Query("UPDATE account set SavingAccount=1")
-		if  error != nil {
+	if strings.Split(qr, "|")[1] == "saving_account" {
+		queryString := fmt.Sprintf("UPDATE account set SavingAccount=1 WHERE AccountID='%s'", account)
+		rows, error := db.Query(queryString)
+		if error != nil {
 			return false
 		}
-	}else if qr.Split(qr,"|")[1]=="fixed_account"{
-		rows, error := db.Query("UPDATE account set FixedAccount=1")
-		if  error != nil {
+		rows.Close()
+	} else if strings.Split(qr, "|")[1] == "fixed_account" {
+		queryString := fmt.Sprintf("UPDATE account set FixedAccount=1 WHERE AccountID='%s'", account)
+		rows, error := db.Query(queryString)
+		if error != nil {
 			return false
 		}
+		rows.Close()
 	}
 
-	idqr := GetIDQR(qr);
-	queryString := fmt.Sprintf("INSERT INTO codeUsed (AccountID,CodeID,PointEarn) VALUES ('%s',%d,%d)",account,idqr,point)
+	idqr := GetIDQR(qr)
+	queryString := fmt.Sprintf("INSERT INTO codeUsed (AccountID,CodeID,PointEarn) VALUES ('%s',%d,%d)", account, idqr, point)
 	rows, error := db.Query(queryString)
-	if  error != nil {
+	if error != nil {
 		return false
 	}
 	rows.Close()
-	
+
 	return true
 }
