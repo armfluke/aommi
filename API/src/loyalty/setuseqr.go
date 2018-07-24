630a6e95568dd131a4058a384f81c3276a3b88a9
ap08960695 <big_xxkool@hotmail.com>
2018-07-24 09:56:21 +0700

    [Edited] edited setuseqr.go in case earn savingaccount and fixedaccount

--- API/src/loyalty/setuseqr.go
+++ API/src/loyalty/setuseqr.go
@@ -11,7 +11,18 @@ func SetUseQR(account string, qr string,point int) bool{
 		return false
 	}
 	defer db.Close()
-	
+	if qr.Split(qr,"|")[1]=="saving_account"{
+		rows, error := db.Query("UPDATE account set SavingAccount=1")
+		if  error != nil {
+			return false
+		}
+	}else if qr.Split(qr,"|")[1]=="fixed_account"{
+		rows, error := db.Query("UPDATE account set FixedAccount=1")
+		if  error != nil {
+			return false
+		}
+	}
+
 	idqr := GetIDQR(qr);
 	queryString := fmt.Sprintf("INSERT INTO codeUsed (AccountID,CodeID,PointEarn) VALUES ('%s',%d,%d)",account,idqr,point)
 	rows, error := db.Query(queryString)
