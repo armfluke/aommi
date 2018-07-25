f82860a441ebc3f060b97440ec74abdb3466d343
Akkrawut Khanthawan <akkrawut33588@gmail.com>
2018-07-25 11:58:55 +0700

    [Edited]: add order by in query sql

--- API/src/loyalty/gethistoryfromdatabase.go
+++ API/src/loyalty/gethistoryfromdatabase.go
@@ -6,22 +6,22 @@ import (
 )
 
 type History struct {
-	Image string `json:"image"`
+	Image  string `json:"image"`
 	Reward string `json:"reward"`
-	Point int `json:"point"`
-	Code string `json:"code"`
-	Date string `json:"date"`
+	Point  int    `json:"point"`
+	Code   string `json:"code"`
+	Date   string `json:"date"`
 }
 
-func GetHistoryFromDatabase(account string) string{
-	
-	db := ConnectDatabase();
-	if db==nil{
+func GetHistoryFromDatabase(account string) string {
+
+	db := ConnectDatabase()
+	if db == nil {
 		return ""
 	}
 	defer db.Close()
-	
-	queryString := fmt.Sprintf("SELECT promotion.Image,promotion.Reward,promotion.Point,promotionUsed.RewardCode,promotionUsed.DateUsed FROM promotionUsed INNER JOIN promotion ON promotion.PromotionID=promotionUsed.PromotionID WHERE promotionUsed.AccountID='%s'",account)
+
+	queryString := fmt.Sprintf("SELECT promotion.Image,promotion.Reward,promotion.Point,promotionUsed.RewardCode,promotionUsed.DateUsed FROM promotionUsed INNER JOIN promotion ON promotion.PromotionID=promotionUsed.PromotionID WHERE promotionUsed.AccountID='%s' ORDER BY promotionUsed.DateUsed DESC", account)
 	rows, _ := db.Query(queryString)
 	if err := rows.Err(); err != nil {
 		return ""
@@ -29,14 +29,13 @@ func GetHistoryFromDatabase(account string) string{
 	var history []History
 	for rows.Next() {
 		var transaction History
-		if error := rows.Scan(&transaction.Image,&transaction.Reward,&transaction.Point,&transaction.Code,&transaction.Date);
-			error != nil {
-			return ""	
+		if error := rows.Scan(&transaction.Image, &transaction.Reward, &transaction.Point, &transaction.Code, &transaction.Date); error != nil {
+			return ""
 		}
-		history = append(history,transaction)
+		history = append(history, transaction)
 	}
 	rows.Close()
-	
-	arrHistory,_ := json.Marshal(history);
+
+	arrHistory, _ := json.Marshal(history)
 	return string(arrHistory)
 }
