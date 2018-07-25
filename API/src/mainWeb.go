c129da788110e5fbb77a82891f75066f415c7760
ap08960695 <big_xxkool@hotmail.com>
2018-07-25 23:26:44 +0700

    [Edited]: web button reset

--- API/src/mainWeb.go
+++ API/src/mainWeb.go
@@ -12,7 +12,6 @@ func main() {
 	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("loyaltyWeb/"))))
 	
 	http.HandleFunc("/web/account", loyaltyWeb.WebViewAccount)
-	http.HandleFunc("/web/account/reset", loyaltyWeb.ResetAccount)
 	http.HandleFunc("/web/account/earn", loyaltyWeb.GetAccountEarn)
 	http.HandleFunc("/web/account/redeem", loyaltyWeb.GetAccountRedeem)
 	http.HandleFunc("/web/qr", loyaltyWeb.GetQR)
