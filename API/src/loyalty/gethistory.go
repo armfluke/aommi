8fa0fdef41f709c88762ccdb1c685351f7222a22
Akkrawut Khanthawan <akkrawut33588@gmail.com>
2018-07-25 11:40:42 +0700

    [Edited]: test gethistory to stub

--- API/src/loyalty/gethistory.go
+++ API/src/loyalty/gethistory.go
@@ -2,7 +2,6 @@ package loyalty
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 )
 
@@ -10,16 +9,17 @@ type PostHistoryData struct {
 	AccountID string `json:accountID`
 }
 
-func GetHistory(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
+type HistoryFunc struct {
+	GetHistoryFromDatabase func(string) string
+}
 
+func (hist HistoryFunc) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 	var data PostHistoryData
 	body := json.NewDecoder(r.Body)
 	body.Decode(&data)
 
-	fmt.Println(data.AccountID)
-
-	message := GetHistoryFromDatabase(data.AccountID)
+	message := hist.GetHistoryFromDatabase(data.AccountID)
 
 	w.Write([]byte(message))
 }
--- API/src/loyalty/gethistory_test.go
+++ API/src/loyalty/gethistory_test.go
@@ -10,13 +10,18 @@ import (
 )
 
 func Test_GetHistory_Json_Input_AccountID_1100400758552_Should_Be_HistoryData(t *testing.T) {
-	expected := `{"statusCode":200,"status":"ok"}`
+	expected := `[{"image":"https://scontent.fbkk8-1.fna.fbcdn.net/v/t1.0-1/p320x320/22894109_221818388356015_7936409751661051386_n.jpg?_nc_cat=0\u0026_nc_eui2=AeEWGQn_zRc0TUUtVqGiznn5mHkxOpmtTCXOFhFjtOzbJwWun-sJ1ugOBN6M-bHlPiIeEVVw-rQTn98r1d1hcLP_HIZkXzx_-bJNN8jEGvuuXw\u0026oh=c24b186755e6aaffc0dbbb01c27e73ec\u0026oe=5BD9CB0A","reward":"ตั๋วหนังMajor","point":100,"code":"MAJ123ABC","date":"2018-07-19 05:13:3}]`
 	msg := `{"accountID":"1100400758552"}`
 
 	r, _ := http.NewRequest(http.MethodPost, "/account/history", strings.NewReader(msg))
 	w := httptest.NewRecorder()
 
-	GetHistory(w, r)
+	stubGetHistory := func(key string) string {
+		return expected
+	}
+	hist := HistoryFunc{stubGetHistory}
+
+	hist.ServeHTTP(w, r)
 
 	resp := w.Result()
 	body, _ := ioutil.ReadAll(resp.Body)
